9515094523f3e544f00434c2fc44c9a63ab6659b
rmatsumoto <rmatsumoto@fine.cs.kobe-u.ac.jp>
2022-07-08 16:00:56 +0900

    期日の追加の実装

--- todoapp/src/main/java/jp/ac/kobe_u/cs/itspecialist/todoapp/dto/ToDoForm.java
+++ todoapp/src/main/java/jp/ac/kobe_u/cs/itspecialist/todoapp/dto/ToDoForm.java
@@ -1,5 +1,9 @@
 package jp.ac.kobe_u.cs.itspecialist.todoapp.dto;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 import javax.validation.constraints.NotBlank;
@@ -7,6 +11,7 @@ import javax.validation.constraints.Size;
 
 import jp.ac.kobe_u.cs.itspecialist.todoapp.entity.ToDo;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 /**
  * ToDoの入力フォーム
@@ -16,12 +21,28 @@ public class ToDoForm {
     @NotBlank
     @Size(min=1, max=64)
     String title; //ToDo題目
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    LocalDateTime due; // 期限
 
     public ToDo toEntity() {
         ToDo t = new ToDo();
         t.setTitle(title);
         t.setCreatedAt(new Date());
         t.setDone(false);
+        t.setDueAt(getDueDate());
         return t;
+    }
+
+    public Date getDueDate() {
+        if (due == null) {
+            return null;
+        }
+        return toDate(due);
+    }
+    private Date toDate(LocalDateTime ldt) {
+        ZoneId id = ZoneId.systemDefault();
+        ZonedDateTime zdt = ZonedDateTime.of(ldt, id);
+        Instant instant = zdt.toInstant();
+        return Date.from(instant);
     }
 }
